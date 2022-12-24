b32ccc0f25cf032c88b412b7d6956619c135b8aa
Imigkos <68186003+Imigkos@users.noreply.github.com>
2022-12-24 14:56:52 +0200

    Add files via upload

--- Shell/header.h
+++ Shell/header.h
@@ -2,9 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define clear() printf("\033[H\033[J")
+#define BUFSIZE 1024
 
 
 void start_shell();
 void print_directory();
 char* getInput();
+char** parseInput(char *input);
+int execute_commands(char **parsed_input);
--- Shell/main.c
+++ Shell/main.c
@@ -1,38 +1,25 @@
 #include "header.h"
-#define BUFSIZE 1024
 
 char *user;
 
 int main()
 {
     char *input_array;
-
+    char **parsed_input = malloc(BUFSIZ * sizeof(char));
     start_shell(); // initialize shell
     while (1)
     {
-        print_directory(); 
+        print_directory();
         input_array = getInput();
-        if (strcasecmp(input_array,"exit") == 0){ //exit shell if user types exit
+        if (strcasecmp(input_array, "exit") == 0)
+        { // exit shell if user types exit
             printf("\n NOW EXITING SHELL");
             return 1;
         }
 
-        // uuuh strtok moment
-        char *cmd = strtok(input_array, " "); //tokenizes input stored in input_array using space, first word sotred in cmd
-        char *argv[BUFSIZE]; // array of individual words from input stored as arguments
-        int argc = 0; //number of arguments
-        while (cmd != NULL) {
-            argv[argc++] = cmd; // stored current value of cmd in argv and increments argc
-            cmd = strtok(NULL, " "); // calls strtok again to get next word, stores it in cmd
-        }
-        argv[argc] = NULL;
-
-        // Execute the command
-        execvp(argv[0], argv);
-
-        printf("\nThats all there is for now ");
+        parsed_input = parseInput(input_array);
+        execute_commands(parsed_input);
     }
-
 }
 
 void start_shell()
@@ -47,19 +34,19 @@ void start_shell()
 
 void print_directory()
 {
-    char dir[BUFSIZE];
+    char dir[1024];
     getcwd(dir, sizeof(dir));
-    fprintf(stderr, "%s@strahd:%s$ ",user, dir);
+    fprintf(stderr, "\n%s@strahd:%s$ ", user, dir);
 }
 
-char* getInput()//read character until EOF or \n is reached and store in array
+char *getInput() // read character until EOF or \n is reached and store in array
 {
-    //int input_array_size = 1024;
+    int input_array_size = 1024;
     int index = 0;
-    char *input_array = malloc(sizeof(char) * BUFSIZE);
+    char *input_array = malloc(sizeof(char) * input_array_size);
     int current;
 
-    if (!input_array) //if error during allocation 
+    if (!input_array) // if error during allocation
     {
         fprintf(stderr, "Error when creating input array\n");
         exit(0);
@@ -68,16 +55,73 @@ char* getInput()//read character until EOF or \n is reached and store in array
     while (1)
     {
         current = getchar();
-        if (current == '\n' || current == EOF)// put trailing newline after EOF
+        if (current == '\n' || current == EOF) // put trailing newline after EOF
         {
             input_array[index] = '\0';
             return input_array;
         }
-        else //store current character in array
+        else // store current character in array
         {
-            input_array[index] = current; 
+            input_array[index] = current;
         }
         index++;
-        
     }
+}
+
+char **parseInput(char *input)
+{
+
+    char *token = strtok(input, " "); // tokenizes input stored in input_array using space, first word sorted in token
+    char **tokens = malloc(BUFSIZ * sizeof(char));
+    // array of individual words from input stored as arguments
+    int position = 0; // number of arguments
+
+    if (!tokens)
+    {
+        fprintf(stderr, "shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    while (token != NULL)
+    {
+        tokens[position++] = token;
+        token = strtok(NULL, " ");
+    }
+
+    token = strtok(NULL, " ");
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int execute_commands(char **parsed_input)
+{
+
+    if (strcmp(parsed_input[0], "cd") == 0)
+    {
+        if (chdir(parsed_input[1]) != 0)
+        {
+            perror("");
+            
+        }
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("Forking child failed\n");
+        return -1;
+    }
+    else if (pid == 0)
+    {
+        if (execvp(parsed_input[0],parsed_input) == -1)
+        {
+            perror("shell");
+        }
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+    }
+    return 1;
 }
