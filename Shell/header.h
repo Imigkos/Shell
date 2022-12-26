5fba7b75b6cac470359f59444e47c3bee9ca8e37
Imigkos <68186003+Imigkos@users.noreply.github.com>
2022-12-26 12:12:10 +0200

    Add files via upload

--- Shell/header.h
+++ Shell/header.h
@@ -4,12 +4,21 @@
 #include<unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+
+
 #define clear() printf("\033[H\033[J")
 #define BUFSIZE 1024
 
+typedef struct command{
+   char** arguments;
+} command;
 
 void start_shell();
 void print_directory();
 char* getInput();
 char** parseInput(char *input);
 int execute_commands(char **parsed_input);
+void parsePipes(char* input);
+void executePipes(command *commandArray);
+
--- Shell/main.c
+++ Shell/main.c
@@ -13,8 +13,8 @@ int main()
         input_array = getInput();
         if (strcasecmp(input_array, "exit") == 0)
         { // exit shell if user types exit
-            printf("\n NOW EXITING SHELL");
-            return 1;
+            printf("NOW EXITING SHELL...\n");
+            exit(0);
         }
 
         parsed_input = parseInput(input_array);
@@ -29,14 +29,13 @@ void start_shell()
     printf("\n===========SHELL OPENED===========\n");
     user = getenv("USER"); // get username
     fprintf(stderr, "         Welcome %s\n", user);
-    sleep(1);
 }
 
 void print_directory()
 {
     char dir[1024];
     getcwd(dir, sizeof(dir));
-    fprintf(stderr, "%s@strahd:%s$ ", user, dir);
+    fprintf(stderr, "\n%s@strahd:%s$ ", user, dir);
 }
 
 char *getInput() // read character until EOF or \n is reached and store in array
@@ -70,8 +69,8 @@ char *getInput() // read character until EOF or \n is reached and store in array
 
 char **parseInput(char *input)
 {
-
-    char *token = strtok(input, " "); // tokenizes input stored in input_array using space, first word sorted in token
+    int i;
+    // tokenizes input stored in input_array using space, first word sorted in token
     char **tokens = malloc(BUFSIZ * sizeof(char));
     // array of individual words from input stored as arguments
     int position = 0; // number of arguments
@@ -81,8 +80,21 @@ char **parseInput(char *input)
         fprintf(stderr, "shell: allocation error\n");
         exit(EXIT_FAILURE);
     }
+
+    for (i = 0; i < strlen(input); i++)
+    {
+
+        if (input[i] == '|')
+        {
+            parsePipes(input);
+            return NULL;
+        }
+    }
+    char *token = strtok(input, " ");
+
     while (token != NULL)
     {
+
         tokens[position++] = token;
         token = strtok(NULL, " ");
     }
@@ -96,16 +108,21 @@ char **parseInput(char *input)
 int execute_commands(char **parsed_input)
 {
 
+    if (parsed_input == NULL)
+    { // pipe has already been executed so return
+        return 0;
+    }
+
     if (strcmp(parsed_input[0], "cd") == 0)
     {
         if (chdir(parsed_input[1]) != 0)
         {
             perror("");
         }
+        return 1;
     }
 
     pid_t pid = fork();
-    pid = 0;
     if (pid < 0)
     {
         printf("Forking child failed\n");
@@ -113,7 +130,7 @@ int execute_commands(char **parsed_input)
     }
     else if (pid == 0)
     {
-        if (execvp(parsed_input[0],parsed_input) == -1)
+        if (execvp(parsed_input[0], parsed_input) == -1)
         {
             perror("shell");
         }
@@ -123,4 +140,96 @@ int execute_commands(char **parsed_input)
         waitpid(pid, NULL, 0);
     }
     return 1;
+}
+
+void parsePipes(char *input)
+{
+    int pipe_counter = 0;
+    char *buffer;
+    int found_pipe = 0;
+    int argc;
+    command *command_arr = malloc(sizeof(command) * BUFSIZ);
+
+    buffer = strtok(input, " ");
+    while (buffer != NULL)
+    {
+        if (strcmp(buffer, "|") == 0)
+        {
+            buffer = strtok(NULL, " ");
+            found_pipe = 1;
+        }
+        else
+        {
+            argc = 0;
+            command_arr[pipe_counter].arguments = malloc(sizeof(char) * BUFSIZ);
+            command_arr[pipe_counter].arguments[argc++] = buffer;
+            buffer = strtok(NULL, " ");
+            while (strcmp(buffer, "|") != 0)
+            {
+                command_arr[pipe_counter].arguments[argc++] = buffer;
+            }
+            pipe_counter++;
+            found_pipe = 0;
+        }
+        if (!found_pipe)
+        {
+            buffer = strtok(NULL, " ");
+        }
+    }
+    command_arr[pipe_counter].arguments = malloc(sizeof(char) * BUFSIZ);
+    command_arr[pipe_counter].arguments[0] = NULL;
+
+    executePipes(command_arr);
+}
+
+void executePipes(command *commandArray)
+{
+
+    // if (execvp(commandArray[1].cmd, commandArray[1].arguments) == -1)
+    // {
+    //     perror("shell: ");
+    // }
+
+    int fd[2];
+    pid_t pid;
+    if (pipe(fd) < 0)
+    {
+        printf("\nError while initializing pipe");
+        return;
+    }
+    pid = fork();
+    if (pid < 0)
+    {
+        printf("\n Failed forking child");
+    }
+
+    if (pid == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+
+        // close(fd[1]);
+
+        if (execvp(commandArray[0].arguments[0], commandArray[0].arguments) < 0)
+        {
+            printf("\nERRRRRORRRRRR");
+            exit(0);
+        }
+    }
+    else
+    {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[1]);
+
+        if (execvp(commandArray[1].arguments[0], commandArray[1].arguments) < 0)
+        {
+            printf("\nERRRRRORRRRRR");
+            exit(0);
+        }
+        else
+        {
+            wait(NULL);
+            wait(NULL);
+        }
+    }
 }
